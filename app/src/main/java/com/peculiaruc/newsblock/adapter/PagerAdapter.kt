28a80f348440d7665de8deb45cb1a3515cf2edf5
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-06-13 23:30:11 +0100

    update

--- app/src/main/java/com/peculiaruc/newsblock/adapter/PagerAdapter.kt
+++ app/src/main/java/com/peculiaruc/newsblock/adapter/PagerAdapter.kt
@@ -3,48 +3,34 @@ package com.peculiaruc.newsblock.adapter
 import androidx.fragment.app.*
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.peculiaruc.newsblock.fragment.SignInFragment
+import com.peculiaruc.newsblock.fragment.SignUpFragment
 
-class PagerAdapter(activity: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(activity) {
-
-    private val mFragmentList: MutableList<Fragment> = ArrayList()
-    private val mFragmentTitleList: MutableList<String> = ArrayList()
+class PagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
 
 
-    fun addFragment(fragment: Fragment, title: String) {
-        mFragmentList.add(fragment)
-        mFragmentTitleList.add(title)
+    override fun getItem(position: Int): Fragment {
+        return when (position){
+           0-> {
+              SignInFragment()
+           }else -> {
+               return SignUpFragment()
+            }
+        }
     }
 
-    override fun getItemCount(): Int {
-        return mFragmentList.size
+    override fun getCount(): Int {
+     return 2
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return mFragmentList[position]
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "SignIn"
+            else -> {
+                return "SignUp"
+            }
+        }
     }
-//
-//    override fun getItem(position: Int): Fragment {
-//        return when (position){
-//           0-> {
-//              SignInFragment()
-//           }else -> {
-//               return SignUpFragment()
-//            }
-//        }
-//    }
-//
-//    override fun getCount(): Int {
-//     return 2
-//    }
-//
-//    override fun getPageTitle(position: Int): CharSequence? {
-//        return when (position) {
-//            0 -> "SignIn"
-//            else -> {
-//                return "SignUp"
-//            }
-//        }
-//    }
-//
+
 
 }
