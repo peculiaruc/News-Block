90ad0e220af1c7ca015562bf893fce4632a2dbcd
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-06-14 23:01:30 +0100

    update

--- app/src/main/java/com/peculiaruc/newsblock/MainActivity.kt
+++ app/src/main/java/com/peculiaruc/newsblock/MainActivity.kt
@@ -4,32 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.peculiaruc.newsblock.adapter.PagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewpager2: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewPager: ViewPager2 = findViewById(R.id.viewpager)
+        val viewPager2: ViewPager2 = findViewById(R.id.viewpager)
         val tabLayout: TabLayout = findViewById(R.id.tablayout)
 
-        val fragAdapter  = PagerAdapter(supportFragmentManager)
-        viewPager.adapter = fragAdapter
+        val fragAdapter  = PagerAdapter(this, supportFragmentManager,tabLayout.tabCount)
+        viewPager2.adapter = fragAdapter
 
-        tabLayout.setupWithViewPager(viewPager)
 
-        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
-            tab.text = if(position == 0) {
-                getString(R.string.signin)}
-            else{
-                getString(R.string.signup)
+        //viewpager2.addOnT(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout.addOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    viewpager2.currentItem = tab.position
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab) {
+
+                }
+                override fun onTabReselected(tab: TabLayout.Tab) {
+
+                }
             }
-        }.attach()
+        )
     }
 
+//        tabLayout.setupWithViewPager(viewPager2)
+//
+//        TabLayoutMediator(tabLayout, viewPager2) {tab, position ->
+//            tab.text = if(position == 0) {
+//                getString(R.string.signin)}
+//            else{
+//                getString(R.string.signup)
+//            }
+//        }.attach()
+//    }
+//
 
 }
 
--- app/src/main/java/com/peculiaruc/newsblock/adapter/PagerAdapter.kt
+++ app/src/main/java/com/peculiaruc/newsblock/adapter/PagerAdapter.kt
@@ -1,36 +1,53 @@
 package com.peculiaruc.newsblock.adapter
 
 import androidx.fragment.app.*
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.peculiaruc.newsblock.fragment.SignInFragment
 import com.peculiaruc.newsblock.fragment.SignUpFragment
 
-class PagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+class PagerAdapter(
+    activity: FragmentActivity,
+    supportFragmentManager: FragmentManager,
+    private val tabCount: Int
+): FragmentStateAdapter(activity) {
 
+    override fun getItemCount(): Int {
+       return tabCount
+    }
 
-    override fun getItem(position: Int): Fragment {
-        return when (position){
-           0-> {
-              SignInFragment()
-           }else -> {
-               return SignUpFragment()
+    override fun createFragment(position: Int): Fragment {
+        when (position){
+            0 -> {
+                return SignUpFragment()
+            }else -> {
+            return SignInFragment()
             }
         }
     }
 
-    override fun getCount(): Int {
-     return 2
-    }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            0 -> "SignIn"
-            else -> {
-                return "SignUp"
-            }
-        }
-    }
-
+//    override fun getItem(position: Int): Fragment {
+//        return when (position){
+//           0-> {
+//              SignInFragment()
+//           }else -> {
+//               return SignUpFragment()
+//            }
+//        }
+//    }
+//
+//    override fun getCount(): Int {
+//     return 2
+//    }
+//
+//    override fun getPageTitle(position: Int): CharSequence? {
+//        return when (position) {
+//            0 -> "SignIn"
+//            else -> {
+//                return "SignUp"
+//            }
+//        }
+//    }
+//
 
 }
